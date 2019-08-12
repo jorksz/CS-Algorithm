e775e77955bf1f99471f421888f3b329f84d369e
jorksz <1223360222@qq.com>
2019-08-12 15:51:59 +0800

    从尾到头打印链表

---
+++ src/main/java/com/offer/linked_list/FromRearToHeadPrintLinkedList.java
@@ -0,0 +1,49 @@
+package com.offer.linked_list;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class FromRearToHeadPrintLinkedList {
+    /**
+     * 合理使用递归解法
+     * @param listNode
+     * @return
+     */
+    ArrayList<Integer> res = new ArrayList<>();
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        if(listNode != null){
+            printListFromTailToHead(listNode.next);
+            res.add(listNode.val);
+        }
+        return res;
+
+    }
+
+    /**
+     * 利用栈来解决 先进后出
+     */
+    public ArrayList<Integer> printListFromTailToHead2(ListNode listNode) {
+        if(listNode == null){
+            return new ArrayList<>();
+        }
+
+        Stack<ListNode> node = new Stack<>();
+        ArrayList<Integer> res = new ArrayList<>();
+
+        while(listNode != null){
+            node.push(listNode);
+            listNode = listNode.next;
+        }
+
+        while(!node.isEmpty()){
+            res.add(node.pop().val);
+        }
+
+        return res;
+    }
+
+    /**
+     * 直接遍历链表，用集合记录数值，最后逆置集合即可
+     */
+
+}
---
+++ src/main/java/com/offer/linked_list/ListNode.java
@@ -0,0 +1,10 @@
+package com.offer.linked_list;
+
+public class ListNode {
+
+    int val;
+    ListNode next = null;
+    ListNode(int val) {
+        this.val = val;
+      }
+}
