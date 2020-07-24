4a7d63909eeca86c08113d87f012c8faace79ac1
jorksz <1223360222@qq.com>
2020-07-24 22:53:46 +0800

    除数博弈

--- src/main/java/leetcode/array/MaximumLengthofRepeatedSubarray.java
+++ src/main/java/leetcode/array/MaximumLengthofRepeatedSubarray.java
@@ -1,7 +1,17 @@
 package leetcode.array;
 
 public class MaximumLengthofRepeatedSubarray {
-   /* public int findLength(int[] A, int[] B) {
-
-    }*/
+    public int findLength(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        int res = 0;
+        int[][] dp = new int[m+1][n+1];
+        for (int i = m - 1; i>=0 ;i--){
+            for (int j = n - 1; j >= 0; j--){
+                dp[i][j] = A[i] == B[j] ? dp[i+1][j+1] + 1 : 0;
+                res = Math.max(dp[i][j], res);
+            }
+        }
+        return res;
+    }
 }
---
+++ src/main/java/leetcode/dp/DivisorGame.java
@@ -0,0 +1,7 @@
+package leetcode.dp;
+
+public class DivisorGame {
+    public boolean divisorGame(int N) {
+        return N % 2 == 0;
+    }
+}
